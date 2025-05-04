4411ce945e2bd5d241956d1d426bc5bad776efc0
pratsy91 <pratapsinghchouhan412@gmail.com>
2025-05-05 02:51:09 +0530

    data updated

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -3,7 +3,7 @@ import { TbBooks } from "react-icons/tb";
 import { BiHappyHeartEyes } from "react-icons/bi";
 
 const data = [
-  { id: 1, icon: <FaAward />, title: "Experience", desc: "1+ Years Working" },
+  { id: 1, icon: <FaAward />, title: "Experience", desc: "2+ Years Working" },
   { id: 2, icon: <TbBooks />, title: "Projects", desc: "10+ Completed" },
   {
     id: 3,
